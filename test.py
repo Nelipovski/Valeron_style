e504570c337e711f49b005adf658d5f2d945377d
Valeron <v.z.nelipovski@mail.ru>
2023-10-18 23:32:38 +0300

    test

---
+++ test.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+conn = sqlite3.connect('example.db')
+
+cursor = conn.cursor()
+
+
+cursor.execute(
+    '''CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)''')
+
+conn.close()
